28121ed1f31dc387afe45feddb15ecd75b3b18c7
x0y14 <ya01ma08@gmail.com>
2022-10-16 16:49:09 +0900

    暗号化したデータをファイルに出力、再読み込みで、正常に復元できるかをテストする関数を追加

--- storage_test.go
+++ storage_test.go
@@ -2,6 +2,7 @@ package pm1
 
 import (
 	"github.com/google/go-cmp/cmp"
+	"os"
 	"testing"
 )
 
@@ -109,4 +110,97 @@ func TestLoadStorageWithEncryption(t *testing.T) {
 	if diff := cmp.Diff(testStorage, restoredTestStorage); diff != "" {
 		t.Errorf("restored storage mismatch (-want +got):\n%s", diff)
 	}
+}
+
+func TestLoadStorageWithEncryptionFromFile(t *testing.T) {
+	// set up testStorage and vault
+	testStorage := NewStorage()
+	testVault := NewVault("test")
+
+	// for example.com
+	// setup generator
+	passwordGen := NewPasswordGenerator()
+	err := passwordGen.Init()
+	if err != nil {
+		t.Errorf("failed to initialize password manager: %v", err)
+	}
+	// password option
+	// in this case, the 40 length password will be generated using upper and lower alphabet and numbers.
+	opt := NewRandomOption(40, true, false, nil)
+	// generate password
+	password, err := passwordGen.GeneratePassword(opt)
+	if err != nil {
+		t.Errorf("failed to generate password: %v", err)
+	}
+
+	conf := NewWebSiteConfidential("example", password, "https://example.com")
+	testVault.Register(conf)
+
+	// register vault onto testStorage
+	testStorage.Register(testVault)
+
+	// dump storage
+	storageBytes, err := DumpStorage(testStorage)
+	if err != nil {
+		t.Errorf("failed to dump storage: %v", err)
+	}
+
+	// encryption
+	masterPassword := "im master password"
+	encryptedStorageBytes, iv, err := Encrypt(storageBytes, masterPassword)
+	if err != nil {
+		t.Errorf("failed to encrypt storage: %v", err)
+	}
+
+	t.Logf("iv = %v\n", iv)
+	t.Logf("encrypted = %v\n", encryptedStorageBytes)
+
+	// write file
+	outputPath := "./secure/storage.enc"
+	if IsExistFile(outputPath) {
+		// file exist
+		f, err := os.Open(outputPath)
+		if err != nil {
+			t.Errorf("failed to open file: %s: %v", outputPath, err)
+		}
+		_, err = f.Write(encryptedStorageBytes)
+		if err != nil {
+			t.Errorf("failed to write encrypted data into(opened): %s: %v", outputPath, err)
+		}
+	} else {
+		// file not exist
+		err = os.WriteFile(outputPath, encryptedStorageBytes, 0755)
+		if err != nil {
+			t.Errorf("failed to write encrypted data into(created): %s: %v", outputPath, err)
+		}
+	}
+
+	// read file
+	readEncryptedStorageBytes, err := os.ReadFile(outputPath)
+	if err != nil {
+		t.Errorf("failed to read encrypted data from: %s: %v", outputPath, err)
+	}
+
+	// decryption
+	decryptedStorageBytes, err := Decrypt(readEncryptedStorageBytes, masterPassword, iv)
+	if err != nil {
+		t.Errorf("faield to decrypt storage: %v", err)
+	}
+
+	// restore storage from bytes
+	restoredTestStorage, err := LoadStorage(decryptedStorageBytes)
+	if err != nil {
+		t.Errorf("failed to restore storage: %v", err)
+	}
+
+	// check diff
+	if diff := cmp.Diff(testStorage, restoredTestStorage); diff != "" {
+		t.Errorf("restored storage mismatch (-want +got):\n%s", diff)
+	}
+
+	err = os.Remove(outputPath)
+	if err != nil {
+		t.Errorf("failed to delete file: %s: %v", outputPath, err)
+	}
+	t.Logf("file deleted: %s", outputPath)
 }
